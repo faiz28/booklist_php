d02f9437ab46d60ef98335168aa6f89c79707018
Faiz Ahmed <31391440+faiz28@users.noreply.github.com>
2022-01-07 20:34:58 +0600

    Update create.php

--- create.php
+++ create.php
@@ -11,8 +11,8 @@ $is_added = 0;
 $pos = -1;
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $new_add = array(
-        "title" =>  preg_replace( "<", "", $_POST['title']),
-        "author" => preg_replace( "<", "",$_POST['author']),
+        "title" =>  preg_replace(  $_POST['title']),
+        "author" => preg_replace($_POST['author']),
         "avilable" => $_POST['avilable'],
         "pages" => $_POST['pages'],
         "isbn" => $_POST['isbn'],
